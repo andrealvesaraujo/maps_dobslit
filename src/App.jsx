e36aea0d3d6572e6c01caffc60a9508e8779a0d0
Andre Alves <andrealvesaraujo.1995@gmail.com>
2022-04-02 11:11:06 -0300

    refactor: remove code about old movingMarker

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,15 @@ import {
   Tooltip,
   TileLayer,
   Marker,
-  useMap
+  useMap,
 } from 'react-leaflet'
 // import { ToastContainer, toast } from 'react-toastify';
 import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
-
-// import { LeafletTrackingMarker } from "react-leaflet-tracking-marker";
-// import ReactLeafletDriftMarker from "react-leaflet-drift-marker";
 
 import 'leaflet-geosearch/dist/geosearch.css';
 import 'react-toastify/dist/ReactToastify.css';
 import 'leaflet/dist/leaflet.css';
 import './App.css';
-
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -38,12 +34,6 @@ const sharedMarkerProps = {
   tooltipAnchor: [16, -28],
 }
 
-// const vehicleMarker = new L.Icon({
-//   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
-//   ...sharedMarkerProps,
-//   className: "movingVehicle"
-// });
-
 const startedMarker = new L.Icon({
   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
   ...sharedMarkerProps,
@@ -55,38 +45,6 @@ const targetMarker = new L.Icon({
 });
 
 const center = [-22.8066667, -43.2105167]
-
-
-// Latitude --> 90 to -90
-// Longitude --> 180 to -180
-// Latitude --> (perto do zero) Norte/Sul (longe do zero), 
-// Longitude --> (esquerda, valores longe do zero) Oeste/Leste (direita, valores perto do zero)
-// const Place1 = [-22.8066, -43.2114]
-// const Place2 = [-22.8066, -43.2105]
-// const Place3 = [-22.8060, -43.2100]
-
-// const SearchStartMarker = () => {
-//   const provider = new OpenStreetMapProvider ();
-
-//   // @ts-ignore
-//   const searchControl = new GeoSearchControl({
-//     searchLabel: 'Starting Address',
-//     notFoundMessage: 'Sorry, that address could not be found.',
-//     marker: {
-//       icon: startedMarker
-//     },
-//     provider: provider,
-//     style: 'bar',
-//   });
-
-//   const map = useMap();
-//   useEffect(() => {
-//     map.addControl(searchControl);
-//     return () => map.removeControl(searchControl);
-//   }, []);
-
-//   return null;
-// };
 
 const SearchNewMarker = ({handlerAddMarker}) => {
   const provider = new OpenStreetMapProvider (
@@ -132,15 +90,11 @@ const SearchNewMarker = ({handlerAddMarker}) => {
 };
 
 const mainPathLineOptions = { color: 'black' }
-let valor = 1
 export default class App extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
-        // vehicleMarkerPositionLatLng: {lat:-22.8066, lng: -43.2114},
-        // placesListIndex: 0,
-        // startedMarkerPoint: [],
         placesMarkerList: [],
         mainPolyline: []
     }
@@ -168,35 +122,6 @@ export default class App extends React.Component {
         ],
       }))
   }
-
-  // startPathHandler = () => {
-  //   const { placesListIndex, placesMarkerList, mainPolyline } = this.state
-  //   if(!mainPolyline.length) {
-  //     toast.error("Click on 'Make Path'", {theme: "colored", autoClose: 2500})
-  //     return 
-  //   }
-  //   let index = placesListIndex
-  //   if(placesListIndex===0){
-  //     index=1
-  //   }
-  //   const target = placesMarkerList[index]
-  //   const targetLatlng = this.pointToLatlng(target) 
-  //   this.setState((state)=> ({
-  //     vehicleMarkerPositionLatLng: targetLatlng,
-  //     placesListIndex: index+1
-  //   }))
-  // }
-
-  // continuePathHandler = ()=> {
-  //   const { placesListIndex, placesMarkerList } = this.state
-  //   if(placesListIndex >= placesMarkerList.length) return
-  //   const target = placesMarkerList[placesListIndex]
-  //   const targetLatlng = this.pointToLatlng(target) 
-  //     this.setState((state)=> ({
-  //       vehicleMarkerPositionLatLng: targetLatlng,
-  //       placesListIndex: placesListIndex+1
-  //   }))
-  // }
   
   makePolylinePath = ()=>{
     this.setState((state)=>({
@@ -208,16 +133,6 @@ export default class App extends React.Component {
     document.location.reload(true);
   }
 
-  // pointToLatlng = (point) => {
-  //     return {
-  //       lat: point[0], lng: point[1]
-  //     }
-  // }
-
-  // latLangToPoint = (latLang) => {
-  //     return [latLang.lat, latLang.lng]
-  // }
-
   render(){
     return (
       <>
@@ -226,39 +141,8 @@ export default class App extends React.Component {
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | <a href="https://dobslit.com/">Dobslit</a>'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          {/* <SearchStartMarker /> */}
           <SearchNewMarker handlerAddMarker={this.addMarker}/>
           <Polyline pathOptions={mainPathLineOptions} positions={this.state.mainPolyline} />
-          {/* <ReactLeafletDriftMarker
-            position={this.state.vehicleMarkerPositionLatLng}
-            duration={5000}
-            eventHandlers={{
-              click: () => {
-                this.startPathHandler()
-              },
-              moveend: ()=>{
-                this.continuePathHandler()
-              }
-            }}
-            icon={vehicleMarker}
-          >
-            <Popup>
-              Main Car<br />
-            </Popup>
-            <Tooltip>Car</Tooltip>
-          </ReactLeafletDriftMarker> */}
-          {/* <Marker position={Place2}>
-            <Popup>
-              Getting Fuel <br />
-            </Popup>
-            <Tooltip>Gas station</Tooltip>
-          </Marker>
-          <Marker position={Place3} icon={targetMarker}>
-            <Popup>
-              End Path.
-            </Popup>
-            <Tooltip>Main Stop</Tooltip>
-          </Marker> */}
           {this.state.placesMarkerList.map((marker, index, arr) =>{
             return (
               <Marker key={`key_${index}`} position={marker.position} icon={index === 0 ? startedMarker : (index === arr.length-1 ? targetMarker : new L.Icon.Default())}>
@@ -270,9 +154,7 @@ export default class App extends React.Component {
             )            
           })}
         </MapContainer>
-        {/* <button className="newMarker" onClick={()=> this.addMarker(this.state.placesMarkerList[this.state.placesMarkerList.length-1][0], this.state.placesMarkerList[this.state.placesMarkerList.length-1][1])}>Add Marker</button> */}
         <button className="primary" onClick={()=> this.makePolylinePath()}>Make Path</button>
-        {/* <button className="secondary" onClick={()=> this.startPathHandler()}>Begin Travel</button> */}
         <button className="resetMap" onClick={()=> this.resetMap()}>Reset Map</button>
       </>
 
